7a2082303da3ffafa21fbb063d70f61f680c0c9a
rpokosov <hasan83842@gmail.com>
2020-05-20 10:50:04 +0300

    add promo script

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -10,3 +10,42 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+function changeClass(makeActive){
+  let promoItems = document.querySelectorAll(".promo__item");
+  promoItems.forEach((el)=>{
+    el.classList.remove("promo__item_active")
+    if(makeActive === true){
+      el.classList.add("promo__item_de-active")
+    }else{
+      el.classList.remove("promo__item_de-active")
+    }
+  });
+}
+
+function toggleIt(event){
+  event.preventDefault();
+  let parent = event.target;
+  if(!parent.classList.contains("promo__item")){
+    return
+  }
+  console.log(parent)
+  // let parent = event.target.closest(".promo__item");
+
+  if(parent.classList.contains("promo__item_active")){
+    changeClass(false)
+    parent.classList.remove("promo__item_de-active")
+    parent.classList.remove("promo__item_active")
+  }else{
+    changeClass(true)
+    parent.classList.remove("promo__item_de-active")
+    parent.classList.add("promo__item_active")
+  }
+
+}
+
+let promoItems = document.querySelectorAll(".promo__item");
+promoItems.forEach((el)=>{
+  // let button = el.querySelector(".promo__item__close");
+  el.addEventListener("click",toggleIt, false);
+});
